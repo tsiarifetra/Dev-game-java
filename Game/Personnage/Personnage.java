fd330bd263a0083a1d851dcbdc16e3c1abea9ace
tsiarifetra <lanilalainamandresy@gmail.com>
2026-02-26 08:29:26 +0300

    first Personnage

---
+++ Game/Personnage/Personnage.java
@@ -0,0 +1,63 @@
+package Game.Personnage;
+
+public class Personnage {
+    String name;
+    int vie;
+    int xp;
+    int level;
+    int atk;
+
+    public Personnage(final String nom){
+        this.name=nom;
+        this.vie=50;
+        this.xp=0;
+        this.level=1;
+        this.atk=5;
+    }
+    public Personnage(final String nom,int V,int X,int level){
+        this.name=nom;
+        this.vie=V;
+        this.xp=X;
+        this.level=level;
+    }
+
+    public int getVie(){
+        return vie;
+    }
+    public int getXp(){
+        return xp;
+    }
+    public int getAtk(){
+        return atk;
+    }
+    public int getLevel(){
+        return level;
+    }
+
+    /////////////
+    public void setVie(int V){
+        vie=V;
+    }
+    public void setXp(int X){
+        xp=X;
+    }
+    public void setAtk(int A){
+        atk=A;
+    }
+    public void setLevel(int L){
+        level=L;
+    }
+
+    public void Attaque(Personnage enemie){
+        System.out.println(name+"attaque"+enemie.name);
+        enemie.vie-=this.atk;
+    }
+
+    public void AffichageStatistique(){
+        System.out.println("Nom : "+name);
+        System.out.println("Poit de vie : "+vie);
+        System.out.println("Level : "+level);
+        System.out.println("Experiance : "+ xp);
+        System.out.println("point d'attaque : "+atk);
+    }
+}
--- main.java
+++ main.java
@@ -1,3 +1,17 @@
+import Game.Personnage.Personnage;
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+        System.out.println("_____________Creation du personnage______________");
+
+        System.out.print("Entre le votre nom : ");
+        Personnage joueur=new Personnage(s.nextLine());
+
+        joueur.AffichageStatistique();
+        joueur.Attaque(joueur);
+
+    }
     
 }
